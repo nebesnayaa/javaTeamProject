034607f64ea227358166fde4665d05c5a44c9ad3
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2025-01-08 00:50:48 +0200

    Add endpoint to fetch resumes by userId
    /resumes/userId/:userId

--- Backend/src/test/java/javaTeamProject/starterjavaTeamProject/MainVerticleTest.java
+++ Backend/src/test/java/javaTeamProject/starterjavaTeamProject/MainVerticleTest.java
@@ -7,12 +7,15 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import model.ResumeDTO;
+import model.ResumesList;
 import model.UserDTO;
 import services.ResumeService;
 import services.UserService;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -24,6 +27,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.swing.text.html.Option;
+
 @ExtendWith(VertxExtension.class)
 @ExtendWith(MockitoExtension.class)
 public class MainVerticleTest {
@@ -31,23 +36,23 @@ public class MainVerticleTest {
 	@Mock private UserService userService;
 	@Mock private ResumeService resumeService;
 	@InjectMocks private MainVerticle verticle;
-	
+
 	WebClient client;
-	
+
 	@BeforeEach
 	void setup(Vertx vertx, VertxTestContext context) {
 		DeploymentOptions options = new DeploymentOptions();
 		JsonObject verticleConfig = new JsonObject();
 		verticleConfig.put("port", 8080);
 		options.setConfig(verticleConfig);
-		
+
 		client = WebClient.create(vertx);
-		
+
 		vertx.deployVerticle(verticle, options)
 			.onSuccess(result -> context.completeNow())
 			.onFailure(err -> context.failNow(err));
 	}
-	
+
 	@Test
 	void findUserByIdExistsTest (Vertx vertx, VertxTestContext context) {
 		UserDTO user = new UserDTO (1,"email", "password", new Date(), new Date());
@@ -65,7 +70,7 @@ public class MainVerticleTest {
 			});
 		});
 	}
-	
+
 	@Test
 	void findUserByIdDoesNotExistsTest (Vertx vertx, VertxTestContext context) {
 		UserDTO user = new UserDTO (1,"email", "password", new Date(), new Date());
@@ -83,4 +88,27 @@ public class MainVerticleTest {
 			});
 		});
 	}
+
+  @Test
+  void findResumeByUserTest (Vertx vertx, VertxTestContext context) {
+    UserDTO user = new UserDTO(1, "email1", "password1", new Date(), new Date());
+    List<ResumeDTO> resumes = List.of(
+      new ResumeDTO(1 ,"content 1", 1, new Date(), new Date(), Optional.of(user)),
+      new ResumeDTO(2 ,"content 2", 2, new Date(), new Date(), Optional.of(user)),
+      new ResumeDTO(3, "content 3", 1, new Date(), new Date(), Optional.of(user)),
+      new ResumeDTO(4 ,"content 4", 1, new Date(), new Date(), Optional.of(user))
+    );
+    ResumesList resumesList = new ResumesList(resumes);
+    Mockito.when(resumeService.findResumeByUserId(user.id())).thenReturn(Future.succeededFuture(resumesList));
+    context.verify(()-> {
+      String request = "http://localhost:8080/resumes/userId/" + user.id().toString();
+      System.out.println(request);
+      client.getAbs(request).send()
+        .onFailure(err -> context.failNow(err.getMessage()))
+        .onSuccess(result -> {
+          Assertions.assertEquals(200, result.statusCode());
+          context.completeNow();
+        });
+    });
+  }
 }
