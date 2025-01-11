234da1ca1639c33286d4e6946448d52d0643c63f
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2025-01-11 06:52:42 +0200

    сhange template ID type to UUID

--- Backend/src/main/java/model/Tameplate.java
+++ Backend/src/main/java/model/Tameplate.java
@@ -1,44 +1,41 @@
 package model;
 
 import java.util.List;
+import java.util.UUID;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name ="template")
 public class Tameplate {
-	@Id @GeneratedValue
-	Integer id;
+	@Id @GeneratedValue(strategy = GenerationType.UUID)
+  UUID id;
 	String name;
 	String layout;
-	
+
 	@OneToMany(mappedBy="template")
 	List<Resume> resumes;
-	
-	public Integer getId() {
+
+	public UUID getId() {
 		return this.id;
 	}
-	
-	public void setId(Integer id) {
+
+	public void setId(UUID id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getLayout() {
 		return this.layout;
 	}
-	
+
 	public void setLayout(String layout) {
 		this.layout = layout;
 	}
