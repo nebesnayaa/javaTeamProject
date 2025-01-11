cd3b18ecc9875133b3877e502cb833949a50f3c1
PlayOnGame001 <126243296+PlayOnGame001@users.noreply.github.com>
2025-01-11 12:03:21 +0200

    translate
    eng

--- Frontend/my-registration-app/src/components/shablon/shablon3.tsx
+++ Frontend/my-registration-app/src/components/shablon/shablon3.tsx
@@ -19,31 +19,31 @@ const ResumeTemplate: React.FC<{ data: ResumeData }> = ({ data }) => {
       <div className="resume-header">
         <h1 className="resume-name">{data.fullName}</h1>
         <p className="resume-age-gender">
-          Возраст: {data.age}, Пол: {data.gender === "male" ? "Мужской" : data.gender === "female" ? "Женский" : "Другой"}
+          Age: {data.age}, Пол: {data.gender === "male" ? "Мужской" : data.gender === "female" ? "Женский" : "Другой"}
         </p>
       </div>
 
       <div className="resume-content">
         <div className="resume-left">
           <div className="resume-section">
-            <h3>КОНТАКТЫ</h3>
+            <h3>Contacts</h3>
             <p>{data.contacts}</p>
           </div>
 
           <div className="resume-section">
-            <h3>НАВЫКИ И ДОСТИЖЕНИЯ</h3>
+            <h3>Skills and awards</h3>
             <p>{data.skillsAndAwards}</p>
           </div>
         </div>
 
         <div className="resume-right">
           <div className="resume-section">
-            <h3>ОПЫТ РАБОТЫ</h3>
+            <h3>Work Expirience</h3>
             <p>{data.workExperience}</p>
           </div>
 
           <div className="resume-section">
-            <h3>ОБРАЗОВАНИЕ</h3>
+            <h3>Education</h3>
             <p>{data.education}</p>
           </div>
         </div>
